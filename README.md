41965aaa18ec23dc43f976019c8fbb7b8a40d239
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-09-04 13:19:40 -0400

    added more parameters and better documentation

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # instascrape
-InstaScrape is a tool used to scrape Instagram pages quickly and efficiently.
+InstaScrape is a terminal tool used to scrape Instagram pages quickly and efficiently.
+
+## disclaimer
+This tool is not authorized and may lead to your account being suspended or kicked off
+Instagram. Use this tool at your own risk. I am not responsible for what Instagram does
+if they catch you using this tool.
+
+Your credentials never leave the Instagram servers. If you don't believe me, you can run Wireshark 
+or any other network analysing program to see all outgoing connections are going exclusively to 
+Instagram servers or audit the code yourself.
+
+## common errors
+### ``IgResponseError - 429``
+This means Instagram has detected a high number of requests from your IP address. This can be fixed
+via connecting to a proxy or a VPN like [Windscribe](https://windscribe.com).
+
+## use cases
+- if your friend's reset their accounts often.
+- if a friend is deleting their account soon.
+- if you want a backup of all of the accounts you follow.
+- if you'd like to have a higher quality, uncropped photo than seen on your phone.
 
 ## installation
 1. Install the latest version of NodeJS.
@@ -8,7 +28,11 @@ InstaScrape is a tool used to scrape Instagram pages quickly and efficiently.
 4. Open the ``config.json`` file it generated to configurate the program by entering your username and password.
 
 ## usage
+#### All accounts you follow
 Run ``node instascrape.js`` to scrape all accounts that you follow.
-
-### upcoming paramaters
+#### A certian account you follow
 Run ``node instascrape.js --user [username]`` to scrape certain users.
+
+*You must be following the account to scrape it.*
+#### Scrape your own account
+Run ``node instascrape.js --self`` to scrape your own account.
--- instascrape.js
+++ instascrape.js
@@ -1,8 +1,8 @@
-const { IgApiClient }  = require('instagram-private-api');
+const { IgApiClient, IgResponseError }  = require('instagram-private-api');
 const fs = require("fs");
 const chalk = require("chalk");
 const req = require("request");
-const url = require("url");
+const args = process.argv.slice(2);
 if (!fs.existsSync("config.json")) {console.log(chalk.red("please follow all of the installation instructions @ https://github.com/n0rmancodes/instascrape")); return;}
 const config = JSON.parse(fs.readFileSync("config.json"));
 
@@ -11,6 +11,10 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
 (async () => {
     const ig = new IgApiClient();
     cls();
+    if (args[0] == "--user" && !args[1]) {
+        console.log(chalk.red("ERR!") + chalk.redBright(" This parameter requires an argument like `node instascrape.js --user somecallmenorman`."));
+        return;
+    }
     console.log("generating device id...");
     await ig.state.generateDevice(config.username);
     await ig.simulate.preLoginFlow();
@@ -20,19 +24,164 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
     process.nextTick(async () => {
         await ig.simulate.postLoginFlow();
         console.log("- logged in");
-        console.log("getting your follower list");
-        var fl = await ig.feed.accountFollowing(l.pk).request();
-        console.log("- got list, you are following " + fl.users.length + " people");
-        console.log("getting your follower's posts...");
-        if (!fs.existsSync("./images")) {fs.mkdirSync("./images/");}
-        for (var c in fl.users) {
-            const u = await ig.feed.user(fl.users[c].pk).request();
-            if (!fs.existsSync("./images/" + fl.users[c].username)) {fs.mkdirSync("./images/" + fl.users[c].username)}
-            console.log("- scraping " + fl.users[c].username + "'s posts");
+        if (!args[0]) {
+            console.log("getting your follower list");
+            var fl = await ig.feed.accountFollowing(l.pk).request();
+            console.log("- got list, you are following " + fl.users.length + " people");
+            console.log("getting your follower's posts...");
+            if (!fs.existsSync("./images")) {fs.mkdirSync("./images/");}
+            for (var c in fl.users) {
+                const u = await ig.feed.user(fl.users[c].pk).request();
+                if (!fs.existsSync("./images/" + fl.users[c].username)) {fs.mkdirSync("./images/" + fl.users[c].username)}
+                console.log("- scraping " + fl.users[c].username + "'s posts");
+                for (var d in u.items) {
+                    if (u.items[d].carousel_media) {
+                        // carousel media
+                        var foldName = "./images/" + fl.users[c].username + "/" + u.items[d].id;
+                        if (fs.existsSync(foldName)) {
+                            console.log("[i] already downloaded " + u.items[d].id);
+                            continue;
+                        } else {
+                            for (var e in u.items[d].carousel_media) {
+                                if (u.items[d].carousel_media[e].image_versions2 && !u.items[d].carousel_media[e].video_versions) {
+                                    // carousel images
+                                    if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
+                                    var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".jpg";
+                                    var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                    if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                                    var bestUrl = u.items[d].carousel_media[e].image_versions2.candidates[0].url;
+                                    var fn = fs.createWriteStream(fileName);
+                                    fs.appendFileSync(fileName, "");
+                                    req(bestUrl).on("error", function(e) {
+                                        //ignore errors
+                                    }).pipe(fn);
+                                    console.log(itemId);
+                                } else if (u.items[d].carousel_media[e].video_versions) {
+                                    // carousel videos
+                                    if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
+                                    var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".mp4";
+                                    var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                    var bestUrl = u.items[d].carousel_media[e].video_versions[0].url;
+                                    var fn = fs.createWriteStream(fileName);
+                                    fs.appendFileSync(fileName, "");
+                                    req(bestUrl).on("error", function(e) {
+                                        //ignore errors
+                                    }).pipe(fn);
+                                    console.log(itemId);
+                                }
+                            }
+                        }
+                    } else if (u.items[d].image_versions2 && u.items[d].video_versions) {
+                        // single videos 
+                        var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".mp4";
+                        if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                        var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;                                
+                        var bestUrl = u.items[d].video_versions[0].url;
+                        var fn = fs.createWriteStream(fileName);
+                        fs.appendFileSync(fileName, "");
+                        req(bestUrl).on("error", function(e) {
+                            //ignore errors
+                        }).pipe(fn);
+                        console.log(itemId);
+                    } else if (u.items[d].image_versions2 && !u.items[d].video_versions) {
+                        // single images
+                        var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".jpg";
+                        var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;
+                        if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                        var bestUrl = u.items[d].image_versions2.candidates[0].url;
+                        var fn = fs.createWriteStream(fileName);
+                        fs.appendFileSync(fileName, "");
+                        req(bestUrl).on("error", function(e) {
+                            //ignore errors
+                        }).pipe(fn);
+                        console.log(itemId);
+                    }
+                }
+            }
+        } else if (args[0] == "--user" && args[1]) {
+            const fl = await ig.feed.accountFollowing(l.pk).request();
+            const user = args[1].toLocaleLowerCase();
+            console.log("checking your follow list...")
+            for (var c in fl.users) {
+                if (fl.users[c].username == user) {
+                    const u = await ig.feed.user(fl.users[c].pk).request();
+                    if (!fs.existsSync("./images/" + fl.users[c].username)) {fs.mkdirSync("./images/" + fl.users[c].username)}
+                    console.log("- scraping " + fl.users[c].username + "'s posts");
+                    for (var d in u.items) {
+                        if (u.items[d].carousel_media) {
+                            // carousel media
+                            var foldName = "./images/" + fl.users[c].username + "/" + u.items[d].id;
+                            if (fs.existsSync(foldName)) {
+                                console.log("[i] already downloaded " + u.items[d].id);
+                                continue;
+                            } else {
+                                for (var e in u.items[d].carousel_media) {
+                                    if (u.items[d].carousel_media[e].image_versions2 && !u.items[d].carousel_media[e].video_versions) {
+                                        // carousel images
+                                        if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
+                                        var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".jpg";
+                                        var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                        if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                                        var bestUrl = u.items[d].carousel_media[e].image_versions2.candidates[0].url;
+                                        var fn = fs.createWriteStream(fileName);
+                                        fs.appendFileSync(fileName, "");
+                                        req(bestUrl).on("error", function(e) {
+                                            //ignore errors
+                                        }).pipe(fn);
+                                        console.log(itemId);
+                                    } else if (u.items[d].carousel_media[e].video_versions) {
+                                        // carousel videos
+                                        if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
+                                        var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".mp4";
+                                        var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                        var bestUrl = u.items[d].carousel_media[e].video_versions[0].url;
+                                        var fn = fs.createWriteStream(fileName);
+                                        fs.appendFileSync(fileName, "");
+                                        req(bestUrl).on("error", function(e) {
+                                            //ignore errors
+                                        }).pipe(fn);
+                                        console.log(itemId);
+                                    }
+                                }
+                            }
+                        } else if (u.items[d].image_versions2 && u.items[d].video_versions) {
+                            // single videos 
+                            var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".mp4";
+                            if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                            var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;                                
+                            var bestUrl = u.items[d].video_versions[0].url;
+                            var fn = fs.createWriteStream(fileName);
+                            fs.appendFileSync(fileName, "");
+                            req(bestUrl).on("error", function(e) {
+                                //ignore errors
+                            }).pipe(fn);
+                            console.log(itemId);
+                        } else if (u.items[d].image_versions2 && !u.items[d].video_versions) {
+                            // single images
+                            var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".jpg";
+                            var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;
+                            if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
+                            var bestUrl = u.items[d].image_versions2.candidates[0].url;
+                            var fn = fs.createWriteStream(fileName);
+                            fs.appendFileSync(fileName, "");
+                            req(bestUrl).on("error", function(e) {
+                                //ignore errors
+                            }).pipe(fn);
+                            console.log(itemId);
+                        }
+                    }
+                } else {
+                    continue;
+                }
+            } 
+        } else {
+            const u = await ig.feed.user(l.pk).request();
+            if (!fs.existsSync("./images/" + l.username)) {fs.mkdirSync("./images/" + l.username)}
+            console.log("scraping " + l.username + "'s posts")
             for (var d in u.items) {
                 if (u.items[d].carousel_media) {
                     // carousel media
-                    var foldName = "./images/" + fl.users[c].username + "/" + u.items[d].id;
+                    var foldName = "./images/" + l.username + "/" + u.items[d].id;
                     if (fs.existsSync(foldName)) {
                         console.log("[i] already downloaded " + u.items[d].id);
                         continue;
@@ -40,9 +189,9 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
                         for (var e in u.items[d].carousel_media) {
                             if (u.items[d].carousel_media[e].image_versions2 && !u.items[d].carousel_media[e].video_versions) {
                                 // carousel images
-                                if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
-                                var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".jpg";
-                                var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                if (!fs.existsSync("./images/" + l.username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + l.username + "/" + u.items[d].id + "/")}
+                                var fileName = "./images/" + l.username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".jpg";
+                                var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + l.username + " (carousel media)";
                                 if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
                                 var bestUrl = u.items[d].carousel_media[e].image_versions2.candidates[0].url;
                                 var fn = fs.createWriteStream(fileName);
@@ -53,9 +202,9 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
                                 console.log(itemId);
                             } else if (u.items[d].carousel_media[e].video_versions) {
                                 // carousel videos
-                                if (!fs.existsSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + fl.users[c].username + "/" + u.items[d].id + "/")}
-                                var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".mp4";
-                                var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + fl.users[c].username + " (carousel media)";
+                                if (!fs.existsSync("./images/" + l.username + "/" + u.items[d].id + "/")) {fs.mkdirSync("./images/" + l.username + "/" + u.items[d].id + "/")}
+                                var fileName = "./images/" + l.username + "/" + u.items[d].id + "/" + u.items[d].carousel_media[e].id + ".mp4";
+                                var itemId = "[i] downloaded " + u.items[d].carousel_media[e].id + " from " + l.username + " (carousel media)";
                                 var bestUrl = u.items[d].carousel_media[e].video_versions[0].url;
                                 var fn = fs.createWriteStream(fileName);
                                 fs.appendFileSync(fileName, "");
@@ -68,9 +217,9 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
                     }
                 } else if (u.items[d].image_versions2 && u.items[d].video_versions) {
                     // single videos 
-                    var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".mp4";
+                    var fileName = "./images/" + l.username + "/" + u.items[d].id + ".mp4";
                     if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
-                    var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;                                
+                    var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + l.username;                                
                     var bestUrl = u.items[d].video_versions[0].url;
                     var fn = fs.createWriteStream(fileName);
                     fs.appendFileSync(fileName, "");
@@ -80,8 +229,8 @@ if (config.username == "" | config.password == "") {console.log(chalk.red("pleas
                     console.log(itemId);
                 } else if (u.items[d].image_versions2 && !u.items[d].video_versions) {
                     // single images
-                    var fileName = "./images/" + fl.users[c].username + "/" + u.items[d].id + ".jpg";
-                    var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + fl.users[c].username;
+                    var fileName = "./images/" + l.username + "/" + u.items[d].id + ".jpg";
+                    var itemId = "[i] downloaded " + u.items[d].id.toString() + " from " + l.username;
                     if (fs.existsSync(fileName)) {console.log("[i] already downloaded " + u.items[d].id); continue;}
                     var bestUrl = u.items[d].image_versions2.candidates[0].url;
                     var fn = fs.createWriteStream(fileName);
